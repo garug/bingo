c82bb2fa3cc8e929c6e13383ecb4586ca7bc2c61
Allef Santana <allefff@gmail.com>
2024-10-26 22:15:30 -0300

    test number 6

--- web/src/routes/+layout.server.ts
+++ web/src/routes/+layout.server.ts
@@ -3,5 +3,7 @@ import type { RequestEvent } from "@sveltejs/kit";
 export async function load({ cookies }: RequestEvent) {
   const credential = cookies.get("credential");
 
+  console.log({ cookies: cookies.getAll() });
+
   return { credential, cookies: cookies.getAll() };
 }
