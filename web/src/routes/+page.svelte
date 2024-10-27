0c782764f6a9eadbc5dce2ab14ecdb8da750ea17
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2024-10-27 16:43:26 -0300

    feat(structure) clean code and initial screen

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import welcomeFallback from "$lib/images/svelte-welcome.png";
-  import welcome from "$lib/images/svelte-welcome.webp";
-  import Counter from "./Counter.svelte";
+  import logo from "$lib/images/logo.png";
 </script>
 
 <svelte:head>
@@ -10,26 +8,26 @@
 </svelte:head>
 
 <section>
-  <h1 class="text-3xl font-bold underline">
-    <span class="welcome">
-      <picture>
-        <source srcset={welcome} type="image/webp" />
-        <img src={welcomeFallback} alt="Welcome" />
-      </picture>
-    </span>
-
-    to your new<br />SvelteKit app
-  </h1>
-
-  <h2>
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
-
-  <Counter />
+  <div class="min-h-screen flex justify-center items-center flex-col gap-4">
+    <img class="w-72" alt="Logo_Bingo" src={logo} />
+    <input
+      type="text"
+      placeholder="Nome da sua sala"
+      class="rounded px-4 py-3 border border-gray-200 peer-focus:after:border-gray-300 outline-gray-300"
+    />
+    <div class="flex flex-col gap-2">
+      <button
+        type="submit"
+        class="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-semibold px-6 py-3 rounded-xl shadow-md"
+      >
+        Iniciar Sorteio
+      </button>
+      <button
+        type="button"
+        class="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-semibold px-6 py-3 rounded-xl shadow-md"
+      >
+        Conectar a Sala
+      </button>
+    </div>
+  </div>
 </section>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
