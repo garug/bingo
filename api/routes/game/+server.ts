ff991a9c5485b4331e499019893d3116470edab4
Allef Santana <allefff@gmail.com>
2024-11-02 15:40:11 -0300

    feat: using authenticate to secure endpoint

--- api/routes/game/+server.ts
+++ api/routes/game/+server.ts
@@ -1,4 +1,11 @@
+import { authenticate } from "@lib/auth.ts";
+import { statusCode } from "@lib/statusCode.ts";
+
 export async function POST(req: Request) {
+  const err = await authenticate(req);
+
+  if (err) return new Response(err, { status: statusCode(err) });
+
   return new Response("Sucess", {
     status: 200,
   });
