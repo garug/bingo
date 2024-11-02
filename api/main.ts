edf119e133cac0e9fca03929cbb39d06f4df0d2d
Allef Santana <allefff@gmail.com>
2024-11-02 11:58:46 -0300

    feat: initial version of file-based url matching

--- api/main.ts
+++ api/main.ts
@@ -1,5 +1,21 @@
-Deno.serve((_req) => {
-  return new Response("Not found", {
-    status: 404,
+Deno.serve(async (req) => {
+  const url = new URL(req.url);
+
+  let module;
+
+  try {
+    module = await import(`./routes${url.pathname}/+server.ts`);
+  } catch (_error) {
+    return new Response("Not found", {
+      status: 404,
+    });
+  }
+
+  if (module[req.method]) {
+    return module[req.method](req);
+  }
+
+  return new Response("Method not implemented", {
+    status: 501,
   });
 });
