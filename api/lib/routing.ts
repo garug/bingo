526f858a9273cd479447d03b21af77603c02b914
Allef Santana <allefff@gmail.com>
2024-11-09 10:50:51 -0300

    feat: initial version of query parameters

--- api/lib/routing.ts
+++ api/lib/routing.ts
@@ -5,6 +5,8 @@ export type RoutePathParameter = Readonly<{
 
 export type PathParameters = Record<string, string | undefined>;
 
+export type QueryParameters = Record<string, string | string[] | undefined>;
+
 export type Route = Readonly<{
   name: string;
   regExp: RegExp;
@@ -20,4 +22,21 @@ export function usePathParameters(req: Request, route: Route) {
     acc[param.key] = pathSegments[param.idx];
     return acc;
   }, {});
+}
+
+export function useQueryParameters(req: Request) {
+  const url = new URL(req.url);
+  const parameters: QueryParameters = {};
+
+  for (const [key, value] of url.searchParams.entries()) {
+    if (parameters[key] === undefined) {
+      parameters[key] = value;
+    } else if (typeof parameters[key] === "string") {
+      parameters[key] = [parameters[key] as string, value];
+    } else {
+      (parameters[key] as string[]).push(value);
+    }
+  }
+
+  return parameters;
 }
