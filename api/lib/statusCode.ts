4b72bb746b90e824dc90fde9eeb1b85448ae7eda
Allef Santana <allefff@gmail.com>
2024-11-10 19:43:39 -0300

    feat: initial version of http responses

--- api/lib/statusCode.ts
+++ api/lib/statusCode.ts
@@ -6,3 +6,17 @@ knownErrors.set("unauthorized", 401);
 export function statusCode(str: string): number {
   return knownErrors.get(str.toLowerCase()) || 400;
 }
+
+export class HttpResponses {
+  static INTERNAL = new Response("Internal server error", {
+    status: 500,
+  });
+
+  static NOT_FOUND = new Response("Not found", {
+    status: 404,
+  });
+
+  static NOT_IMPLEMENTED = new Response("Method not implemented", {
+    status: 501,
+  });
+}
