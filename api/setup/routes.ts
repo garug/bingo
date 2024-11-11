4490ce6fcd17151745b863f8d5aee9d2f8c6dbfb
Allef Santana <allefff@gmail.com>
2024-11-11 11:30:55 -0300

    fix: trying to fix bug on github deploy integration[2]

--- api/setup/routes.ts
+++ api/setup/routes.ts
@@ -3,6 +3,7 @@ import type { RoutePathParameter } from "@lib/routing.ts";
 import { Route } from "@lib/routing.ts";
 import { walk } from "@std/fs";
 import { join, SEPARATOR } from "@std/path";
+import { env } from "process";
 
 const serverRegExp = /(?<=.*)\+server\.ts\b/;
 
@@ -44,7 +45,10 @@ function dirEntryToRoute(dirEntry: WalkEntry, rootLevel: number): Route {
   };
 }
 
-const defaultRoutes = [".", "routes"];
+const defaultRoutes = Deno.env.get("PATH_ROUTES")?.split(",") || [
+  ".",
+  "routes",
+];
 
 export async function setupRoutes(...path: string[]) {
   const iterateOverRoutes = walk(join(...(path || defaultRoutes)), {
